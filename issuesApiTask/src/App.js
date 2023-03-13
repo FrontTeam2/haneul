5a5bae426f275078678f71ec159d0887e09f2c9f
twosky0202 <leehaneul0202@naver.com>
2023-03-14 02:26:56 +0900

    :memo: Init: Init commit

---
+++ issuesApiTask/src/App.js
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import axios from "axios";
+
+function App() {
+    const issuesList = async () => {
+        try {
+            const response = await axios.get(
+                `${process.env.REACT_APP_BACKEND_URL}/repos/angular/angular-cli/issues`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`,
+                    },
+                }
+            );
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        issuesList();
+    }, []);
+
+    return <div>:)</div>;
+}
+
+export default App;
